bb87b580ddd1866a4e97b17a3d629876308070cc
Agastya Rana <agastya.rana@yale.edu>
2023-05-25 09:07:38 -0400

    changed training dict formate

--- src/training_scripts/train_rnn_baseline.py
+++ src/training_scripts/train_rnn_baseline.py
@@ -27,18 +27,18 @@ plume_dict = {
 }
 
 state_dict = {
-    "USE_COSINE_AND_SIN_THETA": True,
+    "USE_COSINE_AND_SIN_THETA": False,
 	"CONCENTRATION_BASE_THRESHOLD": 100, #this is the value that's good for movies. Do not change this to account for normalization-this happens internally.  
 	"CONCENTRATION_THRESHOLD_STYLE": "fixed",
-	"FEATURES": ['conc', 'grad', 'hrc'], ## see OdorFeatures class for options
-	"THETA_DISCRETIZATION": 6, ## number of bins of discretizing theta
+	"FEATURES": ['conc_disc', 'grad_disc', 'hrc_disc'], ## see OdorFeatures class for options
+	"THETA_DISCRETIZATION": 8, ## number of bins of discretizing theta
     "NORMALIZE_ODOR_FEATURES": True,
     "TIMESCALES_S": {"FILTER": 0.2, "THRESHOLD": 0.2}, ## timescales for temporal filtering and thresholding of concentration (in adaptive case)
-    "DISCRETE_OBSERVABLES": False
+    "DISCRETE_OBSERVABLES": True
 }
 
 output_dict = {
-    "RENDER_VIDEO": 'trial.mp4', ## name of video file to render to
+    "RENDER_VIDEO": 'trial_disc.mp4', ## name of video file to render to
     'RECORD_SUCCESS': False ## whether to record rewards and number of successful episodes
 }
 
@@ -56,12 +56,20 @@ agent_dict = {
 }
 
 training_dict = {
-    "N_EPISODES": 10000,
-    "GAMMA": 0.95, # Reward temporal discount factor
-	"MIN_EPSILON":0.01, # Asymptote of decaying exploration rate
-    "MAX_ALPHA": 0.1, # Learning rate
-	"MIN_ALPHA": 0.0001, # Learning rate
-    'RNN_HIDDEN_SIZE': 128
+    "n_episodes": 10000,
+    "max_episode_length": 5000,
+    # "lr_schedule": "constant",
+    # "learning_rate": 0.0001,
+    "gamma": 0.99, ## discount factor
+    "gae_lambda": 0.95, ## GAE parameter
+    "clip_range": 0.2, ## clip range for PPO
+    "vf_coef": 0.5, ## value function coefficient in loss factor
+    "ent_coef": 0.01, ## entropy coefficient in loss factor
+    "lstm_hidden_size": 64, ## size of LSTM hidden state
+    "actor_critic_layers": [64, 64], ## MLP layers for actor-critic heads; first dimension should be lstm_hidden_size
+    "n_envs": 8, ## number of parallel environments
+    "n_steps": 128, ## number of steps per environment per update
+    "model_name": "ppo_recurrent_disc", ## name of model to save
 }
 
 config_dict = {"agent": agent_dict, "plume": plume_dict, "state": state_dict, "output": output_dict, "training": training_dict}
@@ -73,24 +81,30 @@ config_dict = {"agent": agent_dict, "plume": plume_dict, "state": state_dict, "o
 rng = np.random.default_rng(seed=0)
 environment = FlyNavigator(rng, config_dict)
 ## Define the model to be run
-model = RecurrentPPO("MlpLstmPolicy", environment, verbose=1, n_steps=128, batch_size=128*8, policy_kwargs={"lstm_hidden_size": training_dict['RNN_HIDDEN_SIZE']})
+model = RecurrentPPO("MlpLstmPolicy", environment, verbose=1, n_steps=training_dict["n_steps"], batch_size=training_dict["n_steps"]*training_dict["n_envs"], 
+policy_kwargs={"lstm_hidden_size": training_dict['lstm_hidden_size'], "net_arch": training_dict['actor_critic_layers']},
+gamma=training_dict['gamma'], gae_lambda=training_dict['gae_lambda'], clip_range=training_dict['clip_range'], vf_coef=training_dict['vf_coef'], ent_coef=training_dict['ent_coef'])
 # Train the model
-model.learn(total_timesteps=training_dict['N_EPISODES']*config_dict["plume"]["STOP_FRAME"])
+model.learn(total_timesteps=training_dict['n_episodes']*training_dict['max_episode_length'])
 # Save the model
-model.save("ppo_recurrent")
+model.save(os.path.join('.', 'src', 'models', 'saved_models', training_dict['model_name']))
 
-# Create a list of environments
-num_envs = 8
-vec_env = model.get_env()
-obs = vec_env.reset()
+## Test the model
+# Create a single environment for rendering
+render_env = gym.make('YourEnvironment')
+obs = render_env.reset()
 # cell and hidden state of the LSTM
 lstm_states = None
-# Episode start signals are used to reset the lstm states
-episode_starts = np.ones((num_envs,), dtype=bool)
-for _ in range(500):
-    action, lstm_states = model.predict(obs, state=lstm_states, episode_start=episode_starts, deterministic=True)
-    obs, rewards, dones, info = vec_env.step(action)
-    episode_starts = dones
-    vec_env.render("human")
-print('here')
-vec_env.close()
+# Episode start signal
+episode_start = True
+for _ in range(3000):
+    action, lstm_states = model.predict(obs, state=lstm_states, episode_start=episode_start, deterministic=True)
+    obs, _, done, _ = render_env.step(action)
+    render_env.render()
+    # If the episode is done, reset the environment
+    if done:
+        obs = render_env.reset()
+        episode_start = True
+    else:
+        episode_start = False
+render_env.close()
