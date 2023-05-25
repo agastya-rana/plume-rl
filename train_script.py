406c8315cf54d055fbeed49be60e903fe9b436b1
Viraaj Jayaram <vrj9@Physics-19-14.stdusr.yale.internal>
2023-05-25 14:07:51 -0400

    includes source reward argument

--- train_script.py
+++ train_script.py
@@ -29,7 +29,8 @@ config_dict = {
 	"NORMALIZE_ODOR_FEATURES": True,
 	"WALK_SPEED_MM_PER_S": 10,
 	"DELTA_T_S": 1/60,
-	"PER_STEP_REWARD": 0,
+	"PER_STEP_REWARD": -1/60,
+	"SOURCE_REWARD": 100,
 	"WITH_ORIENTATION": False,
 	"USE_MOVIE": True,
 	"MOVIE_PATH": None,
@@ -38,13 +39,13 @@ config_dict = {
 	"RESET_FRAME_RANGE": np.array([501,800]),
 	"SOURCE_LOCATION_MM": np.array([30,90]),
 	"GOAL_RADIUS_MM": 10, #success radius in mm
-	"N_EPISODES" : 2000, # How many independently initialized runs to train on
+	"N_EPISODES" : 10000, # How many independently initialized runs to train on
 	"MAX_ALPHA": 0.1, # Learning rate
 	"MIN_ALPHA": 0.0001,
 	"GAMMA":0.95, # Reward temporal discount factor
 	"MIN_EPSILON":0.01, # Asymptote of decaying exploration rate
 	"MIN_RESET_X_MM": 40, # Initialization condition-minimum agent x in mm
-	"INITIAL_MAX_RESET_X_MM": 45,
+	"INITIAL_MAX_RESET_X_MM": 42,
 	"MAX_RESET_X_MM": 300, # Initialization condition-maximum agent x in mm
 	"MIN_RESET_Y_MM": 0,
 	"MAX_RESET_Y_MM": 180,
@@ -66,7 +67,7 @@ rng = np.random.default_rng(seed)
 plume_movie_path = os.path.join('..','src', 'data', 'plume_movies', 'intermittent_smoke.avi')
 
 config_dict['MOVIE_PATH'] = plume_movie_path
-config_dict['N_EPISODES'] = 10000
+
 
 environment = FlyNavigator(rng = rng, config = config_dict)
 
