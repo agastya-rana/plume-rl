d3a8debc8f1f540600ddc21d1e3d3d57818f3876
Viraaj Jayaram <vrj9@Physics-19-14.stdusr.yale.internal>
2023-06-09 18:18:49 -0400

    fixed indentation and save_freq name issue

--- train_script.py
+++ train_script.py
@@ -17,54 +17,54 @@ from stable_baselines3.common.callbacks import BaseCallback
 plume_movie_path = os.path.join('..', 'src', 'data', 'plume_movies', 'intermittent_smoke.avi')
 
 plume_dict = {
-    "MM_PER_PX": 0.2,
-    "MAX_CONCENTRATION": 255,
-    "PLUME_TYPE": 'movie',
-    "MOVIE_PATH": plume_movie_path,
+	"MM_PER_PX": 0.2,
+	"MAX_CONCENTRATION": 255,
+	"PLUME_TYPE": 'movie',
+	"MOVIE_PATH": plume_movie_path,
 	"MIN_FRAME": 500,
 	"STOP_FRAME": 5000,
 	"RESET_FRAME_RANGE": np.array([501, 800]),
 	"SOURCE_LOCATION_MM": np.array([30,90]),
-    "MIN_RESET_X_MM": 40, # Initialization condition-minimum agent x in mm
+	"MIN_RESET_X_MM": 40, # Initialization condition-minimum agent x in mm
 	"INITIAL_MAX_RESET_X_MM": 300,
 	"MAX_RESET_X_MM": 300, # Initialization condition-maximum agent x in mm
 	"MIN_RESET_Y_MM": 0,
 	"MAX_RESET_Y_MM": 180,
-    "SHIFT_EPISODES": 100,
+	"SHIFT_EPISODES": 100,
 	"RESET_X_SHIFT_MM": 5,
-    "INIT_THETA_MIN": 0,
+	"INIT_THETA_MIN": 0,
 	"INIT_THETA_MAX": 2*np.pi,
 	"PX_THRESHOLD": 100,
 }
 
 state_dict = {
-    "USE_COSINE_AND_SIN_THETA": True,
-    "DISCRETE_OBSERVABLES": False,
-    "FEATURES": ['conc', 'grad', 'hrc'], ## see OdorFeatures class for options,
-    "NORMALIZE_ODOR_FEATURES": True,
+	"USE_COSINE_AND_SIN_THETA": True,
+	"DISCRETE_OBSERVABLES": False,
+	"FEATURES": ['conc', 'grad', 'hrc'], ## see OdorFeatures class for options,
+	"NORMALIZE_ODOR_FEATURES": True,
 	"CONCENTRATION_BASE_THRESHOLD": 100, #this is the value that's good for movies. Do not change this to account for normalization-this happens internally.
 	"USE_BASE_THRESHOLD_FOR_MEAN": False,  
 	"CONCENTRATION_THRESHOLD_STYLE": "fixed",
 	"THETA_DISCRETIZATION": 8, ## number of bins of discretizing theta
-    "TIMESCALES_S": {"FILTER": 0.2, "THRESHOLD": 0.2}, ## timescales for temporal filtering and thresholding of concentration (in adaptive case)
-    "FIX_ANTENNA": False, ## whether to fix the antenna to pointing upwind
+	"TIMESCALES_S": {"FILTER": 0.2, "THRESHOLD": 0.2}, ## timescales for temporal filtering and thresholding of concentration (in adaptive case)
+	"FIX_ANTENNA": False, ## whether to fix the antenna to pointing upwind
 }
 
 output_dict = {
-    "RENDER_VIDEO": 'dqn_agent_run.mp4', ## name of video file to render to
-    "RECORD_SUCCESS": True, ## whether to record rewards and number of successful episodes
-    "SAVE_DIRECTORY": './',
+	"RENDER_VIDEO": 'dqn_agent_run.mp4', ## name of video file to render to
+	"RECORD_SUCCESS": True, ## whether to record rewards and number of successful episodes
+	"SAVE_DIRECTORY": './',
 }
 
 agent_dict = {
-    "ANTENNA_LENGTH_MM": 0.41,
+	"ANTENNA_LENGTH_MM": 0.41,
 	"ANTENNA_WIDTH_MM": 0.21,
-    "WALK_SPEED_MM_PER_S": 10,
+	"WALK_SPEED_MM_PER_S": 10,
 	"DELTA_T_S": 1/60,
-    "TURN_ANG_SPEED_RAD_PER_S": 100*np.pi/180,
+	"TURN_ANG_SPEED_RAD_PER_S": 100*np.pi/180,
 	"MIN_TURN_DUR_S": 0.18,
 	"EXCESS_TURN_DUR_S": 0.18,
-    "GOAL_RADIUS_MM": 10, #success radius in mm
+	"GOAL_RADIUS_MM": 10, #success radius in mm
 }
 
 reward_dict = {
@@ -123,11 +123,11 @@ class CustomCallback(BaseCallback):
 		self.save_freq = save_freq
 		self.save_dir = save_dir
 
-    def _on_step(self) -> bool:
-        if self.n_calls % self.check_freq == 0:
-        	n_eps = len(self.training_env.all_episode_rewards)
-        	save_string = "after_"+str(n_eps)
-        	self.model.save(save_dir+save_string)
+	def _on_step(self) -> bool:
+		if self.n_calls % self.save_freq == 0:
+			n_eps = len(self.training_env.all_episode_rewards)
+			save_string = "after_"+str(n_eps)
+			self.model.save(save_dir+save_string)
 
 
 callback = CustomCallback(save_freq = 500000, save_dir = 'models/')
