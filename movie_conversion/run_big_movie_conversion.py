253e4875dc71900a3bf22cf79b4a7a935d6d5e54
Viraaj Jayaram <vrj9@Physics-19-14.stdusr.yale.internal>
2023-06-14 14:06:57 -0400

    needed for intensity scaling of movie

--- movie_conversion/run_big_movie_conversion.py
+++ movie_conversion/run_big_movie_conversion.py
@@ -4,29 +4,40 @@ import os
 import cv2
 import imageio
 
-movie_path = os.path.join('..','src', 'data', 'plume_movies', 'wider_longer_intermittent_2023-6-8_basic_sub.mp4')
+movie_path = os.path.join('..','src', 'data', 'plume_movies', 'longer_wider_intermittent_full_subtraction.mp4')
 video_capture = cv2.VideoCapture(movie_path)
 
-mask = np.load('longer_wider_intermittent_mask.npy')
+#mask = np.load('longer_wider_intermittent_mask.npy')
 
-video_capture.set(cv2.CAP_PROP_POS_FRAMES, 60)
-_, bck_frame = video_capture.read()
-bck_frame = bck_frame[:,:,2]
+#video_capture.set(cv2.CAP_PROP_POS_FRAMES, 60)
+#_, bck_frame = video_capture.read()
+#bck_frame = bck_frame[:,:,2]
 
-output_file = 'longer_wider_intermittent_full_subtraction.mp4'
+output_file = 'longer_wider_intermittent_final.mp4'
+
 frame_rate = video_capture.get(cv2.CAP_PROP_FPS)
 
 writer = imageio.get_writer(output_file, fps=frame_rate)
 
-for i in range(500,590):
+int_list = np.load('source_int_over_time.npy')
+
+good_ints = int_list[150:]
+good_frames = 150 + np.arange(len(good_ints))
+
+m, b = np.polyfit(good_frames, good_ints, 1)
+
+init_val = m*150 + b
+
+for i in range(150,3800):
+
+	scale = init_val/(m*i + b)
 
 	video_capture.set(cv2.CAP_PROP_POS_FRAMES, i)
 	_, frame_1 = video_capture.read()
 	frame_1 = frame_1[:,:,2]
 
-	frame = frame_1.astype(float) - bck_frame.astype(float)
-	frame[frame<0] = 0
-	frame = frame*mask
+	frame_1 = frame_1.astype(float)
+	frame = scale*frame_1
 	frame = frame.astype(np.uint8)
 	writer.append_data(frame)
 
