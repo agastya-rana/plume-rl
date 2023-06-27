17f79653181b7f7273163091b2c3b2bb1914c69c
Viraaj Jayaram <vrj9@Physics-19-14.stdusr.yale.internal>
2023-06-27 15:25:18 -0400

    not rly meaningful changes but just so merge can happen

--- movie_conversion/convert_movie.py
+++ movie_conversion/convert_movie.py
@@ -4,7 +4,9 @@ import os
 import cv2
 from matplotlib.colors import LogNorm
 
-movie_path = os.path.join('..','src', 'data', 'plume_movies', 'wider_longer_intermittent_2023-6-8_basic_sub.mp4')
+"""
+
+movie_path = os.path.join('..','src', 'data', 'plume_movies', 'longer_wider_intermittent_final.mp4')
 video_capture = cv2.VideoCapture(movie_path)
 
 
@@ -16,47 +18,96 @@ plt.imshow(frame_1)
 plt.show()
 
 
-video_capture.set(cv2.CAP_PROP_POS_FRAMES, 400)
+video_capture.set(cv2.CAP_PROP_POS_FRAMES, 150)
 _, frame_2 = video_capture.read()
 frame_2 = frame_2[:,:,2].T
 
 plt.imshow(frame_2)
 plt.show()
 
-sub = frame_2.astype(float) - frame_1.astype(float)
-sub[sub<0] = 0
+print(np.shape(frame_2))
 
-plt.imshow(sub)
-plt.show()
+#sub = frame_2.astype(float) - frame_1.astype(float)
+#sub[sub<0] = 0
+
+#plt.imshow(sub)
+#plt.show()
 
 num_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 print('total frames = ', num_frames)
 print('fps = ', video_capture.get(cv2.CAP_PROP_FPS))
 
-sum_frame = np.zeros(np.shape(frame_1))
+"""
+
+
+int_list = np.load('source_int_over_time.npy')
+
+good_ints = int_list[150:]
+good_frames = np.arange(len(good_ints))
+
+m, b = np.polyfit(good_frames, good_ints, 1)
+
+fit = m*good_frames + b
+
+print("m = ", m)
+print('b = ', b)
+
+plt.plot(good_frames, good_ints)
+plt.ylabel("Source intensity")
+plt.xlabel("Frames")
+plt.title("Video Source Intensity vs Time")
+plt.plot(good_frames, fit)
+plt.show()
+
+
+
+
+
+
+#int_list = np.zeros(3800)
 
 """
 
-for i in range(0,3800):
+max_list = np.zeros(3233)
+
+for i in range(0,3233):
 
 	print(i)
 	video_capture.set(cv2.CAP_PROP_POS_FRAMES, i)
 	_, frame_1 = video_capture.read()
 	frame_1 = frame_1[:,:,2].T
-	sum_frame += frame_1
+	max_list[i] = np.amax(frame_1)
 
+print(np.max(max_list))
+
+plt.hist(max_list)
+plt.show()
+
+"""
+
+#int_list = np.load('source_int_over_time.npy')
+
+#plt.plot(int_list)
+#plt.title('Video Source Intensity over Time')
+#plt.xlabel('frames')
+#plt.ylabel('intensity')
+#plt.show()
+
+"""
 
 mean_frame = (sum_frame/3800).astype(float)
 mean_frame = mean_frame - frame_1.astype(float)
 mean_frame[mean_frame<0] = 0
 
-"""
+
 
 #np.save('longer_wider_intermittent_mean_frame.npy', mean_frame)
 
-"""
-mean_frame = np.load('longer_wider_intermittent_mean_frame.npy')
+
+#mean_frame = np.load('longer_wider_intermittent_mean_frame.npy')
 #mean_frame[mean_frame==0]=np.nan
+
+
 mean_frame = mean_frame/np.max(mean_frame)
 print(np.shape(mean_frame))
 
@@ -81,7 +132,14 @@ factor = y_coords[-1]/74.5**2
 
 mask = (y_arr > factor*x_coords**2)
 
-np.save('longer_wider_intermittent_mask.npy', mask.T)
+#np.save('longer_wider_intermittent_mask.npy', mask.T)
+
+
+"""
+
+
+mean_frame = np.load('longer_wider_intermittent_mean_frame.npy')
+mask = np.load('longer_wider_intermittent_mask.npy')
 
 plt.imshow(mean_frame.T, norm=LogNorm(vmin=0.001, vmax=1))
 
@@ -90,10 +148,25 @@ cbar = plt.colorbar()
 cbar.set_label('intensity')
 plt.show()
 
-plt.imshow(mean_frame.T*mask.T, norm=LogNorm(vmin=0.001, vmax=1))
+
+x = np.arange(np.shape(mean_frame)[1])
+y = np.arange(np.shape(mean_frame)[0])
+
+X, Y = np.meshgrid(x, y)
+
+# Create a contour plot
+plt.contour(X, Y, mean_frame)
+plt.colorbar()  # Add a colorbar
+plt.show()
+
+
+
+
+"""
+plt.imshow(mean_frame.T*mask, norm=LogNorm(vmin=0.001, vmax=1))
 # Add colorbar
 cbar = plt.colorbar()
 cbar.set_label('intensity')
 plt.show()
-
 """
+
